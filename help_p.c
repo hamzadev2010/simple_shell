6dda26d5b375c775c542bcac82cad2ed87e75592
hamza <hamzachanik@outlook.com>
2023-08-22 08:25:42 -0700

    task9

---
+++ help_p.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ * _print - Write an array of characters to the standard output stream.
+ *
+ * This function writes an array of characters standard output strem
+ *
+ * @string: Pointer to the array of characters.
+ *
+ * Return:The number of bytes written. in error return -1
+ */
+int _print(char *string)
+{
+	return (write(STDOUT_FILENO, string, str_length(string)));
+}
+
+/**
+ * _printe - Write an array of characters to the standard error stream.
+ *
+ * This function writes an array of characters to the standard error stream.
+ * It is typically used for displaying error messages.
+ *
+ * @string: Pointer to the array of characters.
+ *
+ * Return: The number of bytes written. error return -1,and set error
+ */
+int _printe(char *string)
+{
+	return (write(STDERR_FILENO, string, str_length(string)));
+}
+
+/**
+ * _print_error - Write an array of characters to the standard error stream.
+ *
+ * This function writes an array of characters to the standard error stream,
+ * which is typically used to display error messages.
+ *
+ * @data: A pointer to the program's data.
+ * @errorcode: The error code to be printed.
+ *
+ * Return: The number of bytes written.error return -1,and set error
+ */
+int _print_error(int errorcode, data_of_program *data)
+{
+	char n_as_string[10] = {'\0'};
+
+	long_to_string((long) data->exec_counter, n_as_string, 10);
+
+	if (errorcode == 2 || errorcode == 3)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->tokens[0]);
+		if (errorcode == 2)
+			_printe(": Illegal number: ");
+		else
+			_printe(": can't cd to ");
+		_printe(data->tokens[1]);
+		_printe("\n");
+	}
+	else if (errorcode == 127)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->command_name);
+		_printe(": not found\n");
+	}
+	else if (errorcode == 126)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->command_name);
+		_printe(": Permission denied\n");
+	}
+	return (0);
+}
