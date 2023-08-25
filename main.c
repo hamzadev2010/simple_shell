64951be13ad3e98b489faac952a116298161bf9b
hamza <hamzachanik@outlook.com>
2023-08-25 08:15:50 -0700

    task0

--- main.c
+++ main.c
@@ -1,28 +1,19 @@
 #include "shell.h"
+
 /**
- * main - Simple Shell main function
- * @ac: Count of argumnents
- * @av: Arguments
- * Return: zero Always succeded.
+ * main - enter point to starting program
+ *
+ * @ac: an arguments counter.
+ * @av: an arguments values.
+ * @env:an  environment.
+ *
+ * Return: zero o on success
  */
-int main(int ac, char **argv)
+int main(int ac, char **av, char **env)
 {
-	char *line = NULL, **command = NULL;
-	int status = 0;
-	(void) ac;
-	while (1)
-	{
-		line = read_line();
-		if (line == NULL) /* reache EOF ctr + D */
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			return (status);
-		}
-command = tokenizer(line);
-		if (!command)
-			continue;
-else
-status = _execute(command, argv);
-	}
+	int status;
+
+	status = start_shell(ac, av, env);
+
+	return (status);
 }
