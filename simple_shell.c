b6e5e64afe18263052be2735596e4b4055c9929f
hamza <hamzachanik@outlook.com>
2023-08-25 19:19:19 -0700

    task

--- simple_shell.c
+++ simple_shell.c
@@ -1,4 +1,23 @@
 #include "shell.h"
+
+/* global variable for ^C handling */
+unsigned int sig_flag;
+
+/**
+ * sig_handler - handles ^C signal interupt
+ * @uuv: unused variable (required for signal function prototype)
+ *
+ * Return: void
+ */
+static void sig_handler(int uuv)
+{
+	(void) uuv;
+	if (sig_flag == 0)
+		_puts("\n$ ");
+	else
+		_puts("\n");
+}
+
 /**
  * main - main function for the shell
  * @argc: number of arguments passed to main
