092ee806573d322614325d382a42cafaff76b037
hamza <hamzachanik@outlook.com>
2023-08-25 19:30:26 -0700

    task

--- simple_shell.c
+++ simple_shell.c
@@ -1,18 +1,14 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
-
 /**
- * sig_handler - handles ^C signal interupt
- * @uuv: unused variable (required for signal function prototype)
+ * sig_handler - handles ^C signal interrupt
+ * @interactive: indicates if the shell is interactive
  *
  * Return: void
  */
-static void sig_handler(int uuv)
+static void sig_handler(int interactive)
 {
-	(void) uuv;
-	if (sig_flag == 0)
+	if (interactive)
 		_puts("\n$ ");
 	else
 		_puts("\n");
@@ -24,7 +20,7 @@ static void sig_handler(int uuv)
  * @argv: array of arguments passed to main
  * @environment: array of environment variables
  *
- * Return: 0 or exit status, or ?
+ * Return: 0 or exit status
  */
 int main(int argc __attribute__((unused)), char **argv, char **environment)
 {
@@ -34,32 +30,33 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 
 	vars.argv = argv;
 	vars.env = make_env(environment);
-	signal(SIGINT, sig_handler);
+	signal(SIGINT, SIG_IGN);
+
 	if (!isatty(STDIN_FILENO))
 		is_pipe = 1;
 	if (is_pipe == 0)
 		_puts("$ ");
-	sig_flag = 0;
+
 	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
 	{
-		sig_flag = 1;
 		vars.count++;
 		vars.commands = tokenize(vars.buffer, ";");
 		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
 		{
 			vars.av = tokenize(vars.commands[i], "\n \t\r");
 			if (vars.av && vars.av[0])
-				if (check_for_builtins(&vars) == NULL)
+				if (check_for_builtins(&vars) == 0)
 					check_for_path(&vars);
 			free(vars.av);
 		}
 		free(vars.buffer);
 		free(vars.commands);
-		sig_flag = 0;
+
 		if (is_pipe == 0)
 			_puts("$ ");
 		vars.buffer = NULL;
 	}
+
 	if (is_pipe == 0)
 		_puts("\n");
 	free_env(vars.env);
